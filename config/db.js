0ee4a341d6a09541f092c57790821aeb4d7cf976
Soham Patel <sohampatel75750@gmail.com>
2021-10-21 14:01:06 -0400

    updated package.jason for heroku

--- config/db.js
+++ config/db.js
@@ -1,4 +1,5 @@
 module.exports=
 {
-    "URI": "mongodb://localhost/book_store"
+    //"URI": "mongodb://localhost/book_store"
+    "URI": "mongodb+srv://sohamp10:Soham16bcer044@mongodbserver.qi9vo.mongodb.net/book_store?retryWrites=true&w=majority"
 }
